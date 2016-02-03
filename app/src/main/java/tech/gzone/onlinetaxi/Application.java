ca5a87cde836d66a61bb58014d0c6e067cb8618b
Sujeeharan <g_sujeeharan@yahoo.com>
2016-02-03 23:26:43 +0530

    GPS DONE

--- app/src/main/java/tech/gzone/onlinetaxi/Application.java
+++ app/src/main/java/tech/gzone/onlinetaxi/Application.java
@@ -8,20 +8,16 @@ import android.os.AsyncTask;
 public class Application extends AsyncTask {
 
         String uplatitude,uplongitude,uplocationid ;
-        void UpdateLocation(String puplatitude,String puplongitude,String puplocationid){
-
+        Application(String puplatitude,String puplongitude,String puplocationid){
             uplatitude=puplatitude;
             uplongitude=puplongitude;
             uplocationid=puplocationid;
-
-
         }
 
         Database c=new Database();
 
         @Override
         protected Object doInBackground(Object[] params) {
-
             c.insert(uplatitude,uplongitude,uplocationid);
             return null;
         }
